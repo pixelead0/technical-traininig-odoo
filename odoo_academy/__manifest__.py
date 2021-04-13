c3839f2c792492c92b8724b4f5df8c694b863303
Adan Garcia <agarciaq@iingen.unam.mx>
2021-04-13 02:52:09 +0000

    [ADD] Basic module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name':'Odoo Academy',
+
+    'description':"""
+        Academy module to manage Training
+    """,
+    'author':'IINGEN',
+    'website':'https://www.odoo.com',
+    'category':'Training',
+    'version':'0.1',
+    'dependes':['base'],
+    'data':[
+        
+    ],
+    'demo':[
+        
+    ],
+}
