81adaebebf82877ebfd7c33baf9cb6089fea02d8
pixelead0 <info@pixelead0.com>
2021-04-21 03:44:51 -0500

    [fix] odoo_academy

--- odoo_academy/models/sale_order.py
+++ odoo_academy/models/sale_order.py
@@ -6,6 +6,8 @@ from odoo import models, fields, api
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
+    client_order_ref = fields.Char(string="Hola", help_text='skdhfsjkdfh', default='sydfilu sdyfisduyfidsu')
+
     session_id = fields.Many2one(comodel_name='academy.session',
                                  string='Related session',
                                  ondelete='set null')
